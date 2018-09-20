dafb1d7bdcc7cfdb0186e5627f8d95d67aaf7e08
Riicha <riicha_r@yahoo.com>
2018-09-20 05:25:13 -0400

    Sep 20 Html,js,xlsToJsonify.py files updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pymongo 
-from flask import(Flask, render_template,jsonify)
-
+from flask import(Flask, render_template,jsonify, request)
+from Data.CountySelection import CountySelection
 #------------------------------------------------------------------------------------#
 # Flask Setup #
 #------------------------------------------------------------------------------------#
@@ -9,62 +9,61 @@ app = Flask(__name__)
 #------------------------------------------------------------------------------------#
 # Local MongoDB connection #
 #------------------------------------------------------------------------------------#
-# conn = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(conn)
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
 
 # # create / Use database
-# # db = client.healthi_db
+db = client.healthi_db
 
 #------------------------------------------------------------------------------------#
 # MLab MongoDB connection #
 #------------------------------------------------------------------------------------#
-conn = 'mongodb://healthi_admin:healthisrs9=@ds255332.mlab.com:55332/healthi_db'
-client = pymongo.MongoClient(conn,ConnectTimeoutMS=30000)
+# conn = 'mongodb://<add user Pwd here>@ds255332.mlab.com:55332/healthi_db'
+# client = pymongo.MongoClient(conn,ConnectTimeoutMS=30000)
+# db = client.get_default_database()
 
-# #Database connection
-db = client.get_default_database()
+#Database connection
+
 # db = client.get_database('healthi_db')
-
 
 # Home Page
 @app.route("/")
 def home():
-    return(render_template("Landing.html"))
+    result = request.form
+    for r in request.form:
+            print(r)
+    return render_template("Landing.html",result = result)
 
-# Route to display all the five attributes to measure health of a county
-@app.route("/routes")
-def routes():
-    sample_list = []
-    Routes_dict = {}
-    Routes_dict['Attributes'] = "/attributes"
-    Routes_dict['States']= "/states"
-    Routes_dict['State County Names']="/countynames/<state>"
-    Routes_dict['State Zscores Countywise'] ="/countyzscores/<state>"
-    Routes_dict['State Details Countywise']="/countyalldetails/<state>"
-    Routes_dict['State Geographical & Demographical Details Countywise'] = "/countygeodetails/<state>"
-    Routes_dict['Ranks & Zscores Details Countywise'] = "/countyrankszscores/<state>"
-    sample_list.append(Routes_dict)
-    return jsonify(sample_list)
+# User selection is sent
+@app.route('/attributeSelection',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      selection = {}
+      for select in result.items():
+            # populate the selection dictionary
+            selection[select[0]] = select[1]
+
+      print('user selection')
+      print(selection)
+    #   Get the Top 3 counties per user selection
+      userPref = CountySelection(selection)
+      
+      userPref = userPref.Selection()
+      print(userPref.to_html())
+    #   print(userPref.to_html())
+    #   Send back the html back to user
+      return render_template("Landing.html",result = userPref.to_html())
 
 # Route to display all the five attributes to measure health of a county
 @app.route("/attributes")
-def attributes(): 
+def attributes():
     sample_list = []
+    # db = client.Category
     for item in db.Category.find():
         for cat in item['cat']:
             sample_list.append(cat)
     return jsonify(sample_list)
-
-# Route to display all the ranks to measure health of a county
-@app.route("/countyrankszscores/<state>")
-def rankszscores(state):
-    sample_list = []
-    County_dict = {}
-    for item in db.CountyRanksZscores.find():
-        if item['State'].lower() == state.lower():
-            County_dict[state.upper()] = item['CountyDetails']
-    sample_list.append(County_dict)    
-    return jsonify(sample_list)    
 
 #Route to display all the states present in the database
 @app.route("/states")
@@ -72,6 +71,7 @@ def state():
     sample_list = []
     states_list = []
     Statedict={}
+    # db = client.State
     for item in db.State.find():
         states_list.append(item['StateName'])
     Statedict['States'] = states_list 
@@ -82,6 +82,7 @@ def state():
 @app.route("/countynames/<state>")
 def county(state):
     sample_list = []
+    # db = client.State
     for item in db.State.find():
         county_list=[]
         County_dict={}
@@ -89,14 +90,15 @@ def county(state):
             State_Counties = item['Counties']
             for County in State_Counties:
                 county_list.append(County['County']['CountyName'])
-            County_dict['CountyNames'] = county_list          
-            sample_list.append(County_dict)
+            County_dict['County Names'] = county_list          
+            sample_list.append({state.upper() : County_dict})
     return jsonify(sample_list)   
 
 #Route to display all the zscores and rank of all the counties for the given state
-@app.route("/countyzscores/<state>")
+@app.route("/zscores/<state>")
 def zscore(state):
     sample_list = []
+    # db = client.State
     for item in db.State.find():
         if item['StateName'].lower() == state.lower():
             State_Counties = item['Counties']
@@ -110,28 +112,11 @@ def zscore(state):
                 }})
     return jsonify(sample_list)            
 
-# Route to display the all the geographical & demographics information about the county in the given state  
-@app.route("/countygeodetails/<state>")
-def geodemo(state):
-    sample_list=[]
-    for item in db.State.find():
-        if item['StateName'].lower() == state.lower():
-            State_Counties = item['Counties']
-            print(State_Counties)
-            for geodemo in State_Counties:
-                sample_list.append({geodemo['County']['CountyName']:{  
-                'Latitude' : geodemo['County']['Latitude'],
-                'Longitude' : geodemo['County']['Longitude'],
-                'TotalArea' : geodemo['County']['TotalArea'],
-                'Population' : geodemo['County']['Population'],
-                'geodemo.CountyWikiLink' : geodemo['County']['CountyWikiLink']
-                }})
-    return jsonify(sample_list)            
-
 # Route to display the all the information about the given state    
-@app.route("/countyalldetails/<state>")
+@app.route("/details/<state>")
 def details(state):
     sample_list = []
+    # db = client.State
     for item in db.State.find():
         Statedetaildict = {}
         if item['StateName'].lower() == state.lower():
@@ -146,16 +131,44 @@ def details(state):
 # Initiate Flask app
 #------------------------------------------------------------------------------------#
 if __name__=="__main__":
-    connect_args={'check_same_thread':False}
+    #connect_args={'check_same_thread':False}
     app.run(debug=True)
 
 #Pragati : 9/14/2018. Updated to create 4 routes and modified 1 route.
-#          Tested mlab cloud mongodb as well as with local mongodb.  
-#Pragati : 9/15/2018. Updated the code and included two routes: /routes to display
-#          all the available routes and /countygeodetails/<state> to display
-#          geographical & demographical information.
-#Supriya : 9/17/2018. Updated line #79 for Java Script Logic; # 42 for correct display 
-#          of county details route.
-#Pragati : 9/19/2018. Updated the code and included one route: 
-#          /countyrankszscores/<state> to display the ranks and zscores of
-#          the counties.
+#Tested mlab cloud mongodb as well as with local mongodb. 
+# Updated & Tested Riicha : 9/15/2018 Web Scaped & Added # 1. Lat & Long of counties.2. Link to Wiki 
+# 3. population & 4. Area etc
+
+
+#    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################
+# from flask import Flask, render_template, request
+# app = Flask(__name__)
+
+# @app.route('/')
+# def student():
+#    return render_template('student.html')
+
+# @app.route('/result',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
+#       return render_template("result.html",result = result)
+
+# if __name__ == '__main__':
+#    app.run(debug = True)
